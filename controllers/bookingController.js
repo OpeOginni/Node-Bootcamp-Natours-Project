b52492d0c4cbfa126f12066f7c96a95d061fa17f
OpeOginni <brightoginni123@gmail.com>
2023-01-09 17:57:24 +0100

    Bug Fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -62,8 +62,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_item_group.total / 100;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
+  const price = session.amount_total / 100;
   console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
